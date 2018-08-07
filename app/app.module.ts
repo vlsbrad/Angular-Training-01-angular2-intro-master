dbf8b5009afbbef7fdc8f66e6aa8504721438072
Karijes <karijes@inbox.lv>
2018-08-07 22:17:21 +0300

    create AppModule and initig it in platform

---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser'
+
+@NgModule({
+  declarations: [],
+  imports: [ BrowserModule ],
+  exports: [],
+  providers: [],
+})
+
+export class AppModule {
+
+
+}
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import { AppModule } from './app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
