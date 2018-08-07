a07c47af7ed16be82b2a227d63e469f32b6cf9ff
Karijes <karijes@inbox.lv>
2018-08-07 23:28:43 +0300

    template as Gravis

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,18 @@ import {Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: "<header><img src='img/angular.svg' /> <h1>{{ title }}</h1></header>"
+  template: `
+  <header>
+    <img src='img/angular.svg' />
+    <h1>{{ title }}</h1>
+    </header>
+
+    <main>
+      <section class="todo-list">
+        <div class="todo-item">Izuchit Angular 2</div>
+      </section>
+    </main>
+  `
 })
 
 export class AppComponent{
