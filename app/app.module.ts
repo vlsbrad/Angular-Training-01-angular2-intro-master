f55cd7c965ed6157271a70216f43b6ee20e17514
Karijes <karijes@inbox.lv>
2018-08-07 23:03:07 +0300

    hello world in angular.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser'
+import { AppComponent } from './app.component';
+
 
 @NgModule({
-  declarations: [],
   imports: [ BrowserModule ],
-  exports: [],
-  providers: [],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 
 export class AppModule {
